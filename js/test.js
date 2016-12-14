3e7c84f01e770f659d3fb13f3c4204e81eff9fab
lsh <liushenghui@zhijin.com>
2016-12-14 13:36:19 +0800

    time

--- js/test.js
+++ js/test.js
@@ -1,3 +1,8 @@
-$(function(){
-	alert('Hello World');
+$(function() {
+	getTime();
+	setInterval(getTime, 1000);
+	function getTime() {
+		var time = new Date();
+		$('#time').text(time.getFullYear()+'-'+(time.getMonth() + 1)+'-'+time.getDate() + ' ' + time.getHours()+':'+time.getMinutes()+':'+time.getSeconds());
+	}
 });
